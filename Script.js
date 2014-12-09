029689cdba32a24688e17673bfe726d89eaaeb08
Marike Grinde <mg222cd@student.lnu.se>
2014-12-09 21:49:17 +0100

    påbörjat jobb med json-fil

--- Script.js
+++ Script.js
@@ -1,3 +1,28 @@
+loadJSON('file.json',
+         function(data) { console.log(data); },
+         function(xhr) { console.error(xhr); }
+);
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+    console.log(xhr);
+}
+
 var map;
 function initialize () {
     var mapOptions = {
